273f84670fbdc1784da24fa81cbabb8b3cbb871c
Liam <165528316+kommermail@users.noreply.github.com>
2025-01-21 05:15:31 -0500

    Update pdexmodel.py
    retool of size and weaknesses of pokemon analysis

--- pdexmodel.py
+++ pdexmodel.py
@@ -1,15 +1,17 @@
-#using kpokedex.sqlite, make a bubble plot of pokemon with number of weaknesses on the X axis
-#and weight on the Y axis with bubble size being height
+#using kpokedex.sqlite, analyze the relationship between number
+#of weaknesses and size of pokemon.
 
 import sqlite3
 import plotly.express as px
 import pandas as pd
 import re
 
+#establish connection to database
 conec=sqlite3.connect('kpokedex.sqlite')
 cur=conec.cursor()
 
-data=cur.execute('''SELECT Pokemon.name, Weaknesses.weaknesses_num, Height.ht, Weight.wt 
+#extract data from database
+allpkmn=cur.execute('''SELECT Pokemon.name, Weaknesses.weaknesses_num, Height.ht, Weight.wt 
                 FROM Pokemon
                     JOIN Weaknesses
                         ON Weaknesses.id = Pokemon.id
@@ -18,22 +20,43 @@ data=cur.execute('''SELECT Pokemon.name, Weaknesses.weaknesses_num, Height.ht, W
                     JOIN Weight
                         ON Weight.id = Height.id''')
 
-df=pd.DataFrame(data).reset_index(drop=True)
+#load into pkmndf
+pkmndf=pd.DataFrame(allpkmn).reset_index(drop=True)
 
-df.columns=['Name','Number of Weaknesses','height','weight']
+#set column names for use in plotly
+pkmndf.columns=['Name','Number of Weaknesses','height','weight']
+
+#data is in string form. use convert function
 def convert_to_number(xs):
     #e.g. 16.0 m to 16.0
     return (float(re.sub(r'[^0-9.]', '', xs)))
     
     
-df['Height (in m)']=df.height.apply(convert_to_number)
-df['Weight (in kg)']=df.weight.apply(convert_to_number)
-print(df)
+pkmndf['Height (in m)']=pkmndf.height.apply(convert_to_number)
+pkmndf['Weight (in kg)']=pkmndf.weight.apply(convert_to_number)
 
-#lambda xs: convert_to_number(xs)
-#lambda xs: 1+ convert_to_number(xs)
+#bigboypkmn will be same data filtered for big boys, or pokemon larger than a large human
+bigboypkmn=cur.execute('''SELECT Pokemon.name, Weaknesses.weaknesses_num, Height.ht, Weight.wt 
+                FROM Pokemon
+                    JOIN Weaknesses
+                        ON Weaknesses.id = Pokemon.id
+                    JOIN Height
+                        ON Height.id = Weaknesses.id
+                    JOIN Weight
+                        ON Weight.id = Height.id WHERE ht>'1.84m';''')
+
+bigpkmndf=pd.DataFrame(bigboypkmn)
+bigpkmndf.columns=['Name','Number of Weaknesses','height','weight']
+bigpkmndf['Height (in m)']=bigpkmndf.height.apply(convert_to_number)
+bigpkmndf['Weight (in kg)']=bigpkmndf.weight.apply(convert_to_number)
  
+#use plotly for analysis: bubble plot, histogram, and density heatmap.
 
-fig=px.scatter(df, x="Number of Weaknesses", y="Weight (in kg)", size="Height (in m)", hover_data="Name")
+fig=px.scatter(pkmndf, x="Number of Weaknesses", y="Weight (in kg)", size="Height (in m)", hover_data="Name")
 fig.show()
 
+fig2=px.histogram(data_frame=bigpkmndf, x="Height (in m)", color="Number of Weaknesses")
+fig2.show()
+
+fig3=px.density_heatmap(data_frame=bigpkmndf, x="Number of Weaknesses", y="Height (in m)", nbinsx=5, nbinsy=10)
+fig3.show()
